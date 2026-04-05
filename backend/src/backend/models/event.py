31a20312a3fd427967f7d10b81f8f38f41f15234
dheameidiyana <meidiyanawindawatidhea@gmail.com>
2026-04-05 17:30:52 +0700

    feat(models): add User Event Registration models

--- backend/src/backend/models/event.py
+++ backend/src/backend/models/event.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Text, SmallInteger, DateTime
 from sqlalchemy.sql import func
@@ -13,25 +14,27 @@ class Event(Base):
     description: Mapped[str | None] = mapped_column(Text, nullable=True)
     quota: Mapped[int | None] = mapped_column(SmallInteger, nullable=True)
 
-    started_at: Mapped[DateTime | None] = mapped_column(
+    started_at: Mapped[datetime | None] = mapped_column(
         DateTime(timezone=True),
         nullable=True
     )
 
-    ended_at: Mapped[DateTime | None] = mapped_column(
+    ended_at: Mapped[datetime | None] = mapped_column(
         DateTime(timezone=True),
         nullable=True
     )
 
-    created_at: Mapped[DateTime] = mapped_column(
+    created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now()
     )
 
-    updated_at: Mapped[DateTime] = mapped_column(
+    updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now(),
         onupdate=func.now()
     )
 
-    registrations = relationship("Registration", back_populates="event")
+    registrations: Mapped[list["Registration"]] = relationship(
+        back_populates="event"
+    )
