b2e58700e37002e7314ab40fc35285854efbb7c4
raihanad3 <raihanadealfattah@gmail.com>
2026-04-05 11:43:10 +0700

    Capslock Nama Tabel

--- backend/src/backend/models/account.py
+++ backend/src/backend/models/account.py
@@ -5,17 +5,17 @@ from sqlalchemy.sql import func
 from backend.db.base_model import Base
 
 class Account(Base):
-    __tablename__ = "account"
+    __tablename__ = "Account"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    role_id: Mapped[int] = mapped_column(ForeignKey("role.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("User.id"))
+    role_id: Mapped[int] = mapped_column(ForeignKey("Role.id"))
     email: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
     username: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(Text, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
 
-    user: Mapped["User"] = relationship(back_populates="accounts")
-    role: Mapped["Role"] = relationship(back_populates="accounts")
-    logs: Mapped[list["Log"]] = relationship(back_populates="account")
+    user: Mapped["User"] = relationship(back_populates="Accounts")
+    role: Mapped["Role"] = relationship(back_populates="Accounts")
+    logs: Mapped[list["Log"]] = relationship(back_populates="Logs")
