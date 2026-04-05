28503977c92ce5f410055af53c818a8efc684da7
raihanad3 <raihanadealfattah@gmail.com>
2026-04-05 13:09:35 +0700

    feat(table): Add Account Log Role models

--- backend/src/backend/db/base.py
+++ backend/src/backend/db/base.py
@@ -1,8 +1,6 @@
 from backend.db.base_model import Base
 from backend.models.item import Item
 from backend.models.role import Role
-from backend.models.user import User
-from backend.models.event import Event
 from backend.models.account import Account
 from backend.models.log import Log
-from backend.models.registration import Registration
+
