3a2578b65b2af3d409c0511c6a71b394ccde9e14
Satria Manggala <satriampp@proton.me>
2026-04-06 15:47:44 +0700

    feat(backend): init router and endpoint

--- backend/src/backend/api/v1/endpoints/account.py
+++ backend/src/backend/api/v1/endpoints/account.py
@@ -1,27 +1,34 @@
-from fastapi import Depends
+from fastapi import Depends, APIRouter
 from sqlmodel import Session
 
-from src.database.connection import get_session
-from src.dto.account import AccountCreate
-from src.services.account_service import (
+from backend.db.session import get_db
+from backend.schemas.account import AccountCreate
+from backend.services.account import (
     create_account_service,
     get_all_accounts_service,
     get_account_by_id_service,
     delete_account_service,
 )
 
+router = APIRouter()
 
-def get_accounts(db: Session = Depends(get_session)):
+
+@router.get("/")
+def get_accounts(db: Session = Depends(get_db)):
     return get_all_accounts_service(db)
 
 
-def get_account(account_id: int, db: Session = Depends(get_session)):
+@router.get("/{account_id}")
+def get_account(account_id: int, db: Session = Depends(get_db)):
     return get_account_by_id_service(db, account_id)
 
 
-def create_account(data: AccountCreate, db: Session = Depends(get_session)):
+@router.post("/")
+def create_account(data: AccountCreate, db: Session = Depends(get_db)):
     return create_account_service(db, data)
 
 
-def delete_account(account_id: int, db: Session = Depends(get_session)):
+@router.delete("/{account_id}")
+def delete_account(account_id: int, db: Session = Depends(get_db)):
     return delete_account_service(db, account_id)
+
--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
+from backend.api.v1.main import api_router
 
 app = FastAPI()
+
+app.include_router(api_router, prefix="/api/v1")
+
+
+@app.get("/")
+def home():
+    return "Welcome"
