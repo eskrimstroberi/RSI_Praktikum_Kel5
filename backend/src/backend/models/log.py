282568ec432e575ac232c6fbfbe3e094f25c4808
Satria Manggala <satriampp@proton.me>
2026-04-06 14:22:02 +0700

    feat(model): add account, item, log, role model

---
+++ backend/src/backend/models/log.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Text, ForeignKey, String, DateTime
+from sqlalchemy.sql import func
+from backend.db.base_model import Base
+
+class Log(Base):
+    __tablename__ = "log"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    account_id: Mapped[int] = mapped_column(ForeignKey("account.id"))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    action: Mapped[str] = mapped_column(String(75), nullable=False)
+    ip_address: Mapped[str | None] = mapped_column(Text, nullable=False)
+    user_agent: Mapped[str | None] = mapped_column(Text, nullable=False)
+    entity: Mapped[str] = mapped_column(String(50), nullable=False)
+    entity_id: Mapped[int | None] = mapped_column(nullable=True)
+    
+    account: Mapped["Account"] = relationship(back_populates="logs")
