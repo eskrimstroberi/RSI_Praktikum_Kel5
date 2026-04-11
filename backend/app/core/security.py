237c3a6f977f9a6ad1f347063991ba8d8a7c2d3c
Satria Manggala <satriampp@proton.me>
2026-04-12 02:00:16 +0700

    feat(auth): add hash password method

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,15 +1,31 @@
 from typing import Any
 from datetime import datetime, timedelta, UTC
-from jose import jwt
+
+import jwt
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 
 from app.core.config import settings
-from app.db.session import get_session
 from app.models.account import Account
+from app.db.session import get_session
 
+ph = PasswordHasher()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/login")
+
+
+def hash_password(password: str) -> str:
+    return ph.hash(password)
+
+
+def verify_password(hashed_password: str, plain_password: str) -> bool:
+    try:
+        return ph.verify(hashed_password, plain_password)
+    except VerifyMismatchError:
+        return False
 
 
 def create_access_token(data: dict[str, Any]):
