3482b568caf903d9c6a2ee3cff6e30c08afddbec
Satria Manggala <satriampp@proton.me>
2026-04-12 08:15:28 +0700

    feat: enumerate role name

--- backend/app/models/role.py
+++ backend/app/models/role.py
@@ -1,10 +1,11 @@
 from sqlmodel import SQLModel, Field, Relationship
+from app.models.enums import RoleName
 
 
 class Role(SQLModel, table=True):
     __tablename__ = "Role"
 
     id: int = Field(primary_key=True, index=True)
-    name: str = Field(max_length=255)
+    name: RoleName = Field(unique=True)
 
     accounts: list["Account"] = Relationship(back_populates="role")
