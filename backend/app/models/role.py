42206290cac7d2a9aa939985ed02700791328631
Satria Manggala <satriampp@proton.me>
2026-04-12 08:48:28 +0700

    fix(model): switch Role.name back to string to fix postgres type error

--- backend/app/models/role.py
+++ backend/app/models/role.py
@@ -1,4 +1,5 @@
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import SQLModel, Field, Relationship, String
+import sqlalchemy as sa
 from app.models.enums import RoleName
 
 
@@ -6,6 +7,8 @@ class Role(SQLModel, table=True):
     __tablename__ = "Role"
 
     id: int = Field(primary_key=True, index=True)
-    name: RoleName = Field(unique=True)
+    name: RoleName = Field(
+        sa_column=sa.Column(sa.String(255), unique=True, nullable=False)
+    )
 
     accounts: list["Account"] = Relationship(back_populates="role")
